71adee6d3b581c35d323e7a40ab74dc7579789f4
Arun <aravind6@its.jnj.com>
2020-02-06 09:46:32 +0100

    This is commit no.2 in feature 1 branch

--- helloworld.cpp
+++ helloworld.cpp
@@ -5,5 +5,7 @@ int main()
 	std::cout<<"Hello World"<<std::endl;
 	//The below LOC is for feature 1 Branch
 	std::cout<<"Hello World from feature 1 branch"<<std::endl;
+	//The below LOC is for feature 1 Branch - commit no.2 in feature1 branch
+	std::cout<<"Hello World. This is commit no.2 in feature 1 branch"<<std::endl;
 	return 0;
 }
